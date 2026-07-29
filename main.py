eee0eeeb3b38d7d026ab927b07444a267cbd9779
folitovohespera <folitovohespera@gmail.com>
2026-07-29 21:55:56 +0000

    feat: refactorisation du code avec les dataclasses pour le jour 3

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
+from dataclasses import dataclass
+@dataclass
+class Produit:
+        nom : str
+        prix: float
+        stock: int
+
 tableau_info_produit = []
 def info_produit():
         nom_produit = input("donner le nom du produit : ")
         prix_produit = int(input("donner le prix du produit : "))
         stock_produit = int(input("donner le stock de ce produit : "))
-        produit = {
-            "nom": nom_produit,
-            "prix": prix_produit,
-            "stock": stock_produit
-        }
+        produit = Produit(nom = nom_produit, prix = prix_produit, stock= stock_produit)
         tableau_info_produit.append(produit)            
         print(f"produit {nom_produit} ajouté avec succes!")
 def afficher_tableau():
         print("-----tableau des produits présentes-----")
-        for i in tableau_info_produit:
-                print(f"produit {i["nom"]}: prix : {i["prix"]}, stock {i["stock"]}")
+        for p in tableau_info_produit:
+                print(f"nom du produit :  {p.nom} | prix du produit : {p.prix} | stock du produit : {p.stock}")
 info_produit()
 afficher_tableau()
 
